aae20e327ca3db20c68e7c7033f50f9bff298468
criniguez <felizpc1@gmail.com>
2021-07-06 11:19:15 -0400

    knowledges styles improved

--- components/sections/Knowledges.jsx
+++ components/sections/Knowledges.jsx
@@ -19,76 +19,20 @@ import classNames from 'classnames';
 import styles from '../../styles/components/sections/Knowledges.module.scss';
 
 const knowledges = [
-  {
-    name: 'HTML',
-    Icon: SiHtml5,
-    starred: true,
-  },
-  {
-    name: 'CSS',
-    Icon: SiCss3,
-    starred: true,
-  },
-  {
-    name: 'JavaScript',
-    Icon: SiJavascript,
-    starred: true,
-  },
-  {
-    name: 'Git',
-    Icon: SiGit,
-    starred: true,
-  },
-  {
-    name: 'NodeJS',
-    Icon: SiNodeDotJs,
-    starred: false,
-  },
-  {
-    name: 'React',
-    Icon: SiReact,
-    starred: true,
-  },
-  {
-    name: 'TypeScript',
-    Icon: SiTypescript,
-    starred: true,
-  },
-  {
-    name: 'Sass',
-    Icon: SiSass,
-    starred: true,
-  },
-  {
-    name: 'Electron',
-    Icon: SiElectron,
-    starred: true,
-  },
-  {
-    name: 'Bootstrap',
-    Icon: SiBootstrap,
-    starred: false,
-  },
-  {
-    name: 'Express',
-    Icon: FaServer,
-    starred: false,
-  },
-  {
-    name: 'MySQL',
-    Icon: SiMysql,
-    starred: false,
-  },
-  {
-    name: 'MongoDB',
-    Icon: SiMongodb,
-    starred: false,
-  },
-  {
-    name: 'Firebase',
-    Icon: SiFirebase,
-    starred: false,
-  },
+  { name: 'HTML', Icon: SiHtml5, starred: true },
+  { name: 'CSS', Icon: SiCss3, starred: true },
+  { name: 'JavaScript', Icon: SiJavascript, starred: true },
+  { name: 'Git', Icon: SiGit, starred: true },
+  { name: 'NodeJS', Icon: SiNodeDotJs, starred: false },
+  { name: 'React', Icon: SiReact, starred: true },
+  { name: 'TypeScript', Icon: SiTypescript, starred: true },
+  { name: 'Sass', Icon: SiSass, starred: true },
+  { name: 'Electron', Icon: SiElectron, starred: true },
+  { name: 'Bootstrap', Icon: SiBootstrap, starred: false },
+  { name: 'Express', Icon: FaServer, starred: false },
+  { name: 'MySQL', Icon: SiMysql, starred: false },
+  { name: 'MongoDB', Icon: SiMongodb, starred: false },
+  { name: 'Firebase', Icon: SiFirebase, starred: false },
 ];
 
 const Knowledge = ({ name, Icon, starred }) => (
@@ -101,7 +45,6 @@ const Knowledge = ({ name, Icon, starred }) => (
 const Knowledges = () => {
   return (
     <section className={classNames(styles.knowledges, 'section')}>
-      {/* <div className='container knowledges__container'> */}
       <div className={classNames(styles.knowledges__container, 'container')}>
         <div className={styles.knowledges__title}>
           <h2 className='section__title'>Mis Conocimientos</h2>
